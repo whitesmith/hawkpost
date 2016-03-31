99dd4a9a74403bcc59e5aa38674524e5f535f6fc
Gonçalo Valério <gon@ovalerio.net>
2016-03-31 21:08:36 +0100

    added email config to production settings

--- postbox/settings/production.py
+++ postbox/settings/production.py
@@ -17,3 +17,12 @@ DATABASES = {
         'PORT': os.environ.get("DB_PORT"),
     }
 }
+
+
+# Email Settings
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
