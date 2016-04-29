754989e97d2f08f8df512444263d62f6ad12ba41
Gonçalo Valério <gon@ovalerio.net>
2016-04-29 19:54:48 +0100

    small fix on the middleware

--- hawkpost/middleware.py
+++ hawkpost/middleware.py
@@ -4,8 +4,6 @@ from django.utils import timezone
 class TimezoneMiddleware():
     def process_request(self, request):
         if request.user.is_authenticated():
-            tzname = request.user.timezone
-            if tzname:
-                timezone.activate(tzname)
-            else:
-                timezone.deactivate()
+            timezone.activate(request.user.timezone)
+        else:
+            timezone.deactivate()
