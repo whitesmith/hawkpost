73e85705c2e8ba6f733322510793f4dfab206875
Gonçalo Valério <gon@ovalerio.net>
2016-05-06 19:00:54 +0100

    added worker job, to check for revoked and expired keys

---
+++ humans/utils.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.utils import timezone
+from datetime import datetime
+
+
+def key_state(key):
+    results = settings.GPG_OBJ.import_keys(key).results
+    keys = settings.GPG_OBJ.list_keys()
+    if not results or not results[0]["fingerprint"]:
+        return None, "invalid"
+    else:
+        state = "valid"
+        result = results[0]
+    for key in keys:
+        if key["fingerprint"] == result["fingerprint"]:
+            exp_timestamp = int(key["expires"]) if key["expires"] else ""
+            expires = datetime.fromtimestamp(exp_timestamp, timezone.utc)
+            if key["trust"] == "r":
+                state = "revoked"
+            elif exp_timestamp and expires < timezone.now():
+                state = "expired"
+            break
+    return result["fingerprint"], state
