9103252e00dd0d566d71dc4829b650d00797400d
Ricardo Pereira <m@ricardopereira.eu>
2016-05-13 19:58:23 +0100

    Validate the expiration date

--- boxes/forms.py
+++ boxes/forms.py
@@ -1,11 +1,25 @@
 from django.forms import ModelForm, Form, CharField, Textarea
 from .models import Box
+import datetime
 
 
 class CreateBoxForm(ModelForm):
     class Meta:
         model = Box
         fields = ["name", "description", "expires_at"]
+
+    def clean_expires_at(self):
+        # Validate the expiration date
+        if self.expires_at:
+            expires_at = self.expires_at
+        else:
+            expires_at = self.cleaned_data.get("expires_at", "")
+
+        if expires_at:
+            # Check if the expiration date is a past date
+            if datetime.now() > expires_at:
+                self.add_error('expires_at', "This expiration date is not valid")
+        return expires_at
 
 
 class SubmitBoxForm(Form):
