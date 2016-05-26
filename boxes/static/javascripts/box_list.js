c5efabe6cd5652e25425eeb94c819fd81b1a340e
Gonçalo Valério <gon@ovalerio.net>
2016-05-26 19:10:05 +0100

    Added button to easily copy to the box url

---
+++ boxes/static/javascripts/box_list.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  /*
+    On click copy the box link to the clipboard
+  */
+  $(".copy-to-clipboard-js").on("click", function(){
+    $this = $(this);
+    var previousText = $this.text();
+    var input = $("#" + $this.attr("data-src"));
+    input.select();
+    try{
+      var result = document.execCommand('copy');
+      if(result){
+        $this.text("Link copied with success");
+      } else {
+        $this.text("Unable to copy to clipboard");
+      }
+      setTimeout(function(){
+        $this.text(previousText);
+      }, 1500);
+    } catch (err) {
+      //Browser does not support "copy"?
+      $this.text("Unable to copy to clipboard");
+      $(".copy-to-clipboard-js").prop("disabled", true);
+    }
+  });
+});
