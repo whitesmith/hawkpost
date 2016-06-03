3f314a9e3973ff02d463b0775ad541ee6f91cfac
Gonçalo Valério <gon@ovalerio.net>
2016-06-03 17:47:31 +0100

    disable expires_at input if never_expires is selected

--- boxes/static/javascripts/box_create.js
+++ boxes/static/javascripts/box_create.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
     $("#id_expires_at").datetimepicker({
       format: "Y-m-d H:i"
     });
+
+    $("#id_never_expires").on("change", function(){
+      $this = $(this);
+      if ($this.is(":checked")){
+        $("#id_expires_at").prop("disabled", true).val("");
+      } else {
+        $("#id_expires_at").prop("disabled", false);
+      }
+    });
 });
