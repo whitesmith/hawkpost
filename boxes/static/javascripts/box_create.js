f5afe61e416e7ab731689bd71f562cb9d30f51d0
Gonçalo Valério <gon@ovalerio.net>
2016-06-03 19:18:21 +0100

    removed global var

--- boxes/static/javascripts/box_create.js
+++ boxes/static/javascripts/box_create.js
@@ -4,8 +4,7 @@ $(document).ready(function() {
     });
 
     $("#id_never_expires").on("change", function(){
-      $this = $(this);
-      if ($this.is(":checked")){
+      if ($(this).is(":checked")){
         $("#id_expires_at").prop("disabled", true).val("");
       } else {
         $("#id_expires_at").prop("disabled", false);
