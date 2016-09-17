ef3a34b7d8bb6f065537e2248b8bce152956120d
Gonçalo Valério <gon@ovalerio.net>
2016-09-17 02:02:08 +0100

    removed all inline scrips and fixed settings html

---
+++ hawkpost/static/javascripts/global.js
@@ -0,0 +1,20 @@
+var polyfilter_scriptpath = '/static/javascripts/';
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $(".messages-elem-js").remove();
+  }, 3000);
+
+  /*
+    On user settings page, open the tooltips/popups
+  */
+  $(".server-signed-info-js").on("click", function () {
+    var popup = document.getElementById('server-signed-content-js');
+    popup.classList.toggle('show');
+  })
+
+  $(".keys-help-popup-js").on("click", function () {
+    var popup = document.getElementById('keys-help-content-js');
+    popup.classList.toggle('show');
+  })
+});
