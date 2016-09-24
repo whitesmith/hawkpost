73bd142e4fffece346368f4181975e748e9a7506
José Ribeiro <hello@jlbribeiro.com>
2016-09-24 03:39:02 +0100

    Even leaner Gulpfile: removing unnecessary tasks

--- gulpfile.js
+++ gulpfile.js
@@ -30,9 +30,6 @@ var moduleImporter = require('sass-module-importer');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 
-/* Images */
-var imagemin = require('gulp-imagemin');
-
 /* Livereload */
 var browserSync = require('browser-sync');
 
@@ -71,7 +68,7 @@ tasks.browser_sync = function () {
         forms: false,
         scroll: false
       }
-  }
+  };
 
   browserSync(config);
 };
@@ -112,38 +109,17 @@ tasks.sass = function () {
     .pipe(browserSync.reload({ stream: true }));
 };
 
-tasks.fonts = function() {
-  return gulp.src(sourcePaths.assets + 'fonts/**/*.*')
-    .pipe(gulp.dest(buildPath + 'assets/fonts/'));
-};
-
-tasks.images = function() {
-  return gulp.src(sourcePaths.assets + 'images/**/*.*')
-  .pipe(gulpif(production, imagemin({ progressive: true })))
-  .pipe(gulp.dest(buildPath + 'assets/images/'));
-};
-
 /* Compilation tasks */
-gulp.task('clean',  tasks.clean);
+gulp.task('clean', tasks.clean);
 
-gulp.task('sass',   tasks.sass);
-gulp.task('views',  tasks.views);
-gulp.task('fonts',  tasks.fonts);
-gulp.task('images', tasks.images);
+gulp.task('sass', tasks.sass);
 
 /* Assets compilation and reload. */
 gulp.task('browser-sync', tasks.browser_sync);
 var browserSyncRefresh = browserSync.reload.bind(browserSync);
 var browserSyncStream  = function () { browserSync.reload({ stream: true }); };
 
-gulp.task('reload-sass',   ['sass'],          browserSyncStream);
-gulp.task('reload-vendor', ['vendor-styles'], browserSyncStream);
-gulp.task('reload-fonts',  ['fonts'],         browserSyncRefresh);
-gulp.task('reload-images', ['images'],        browserSyncRefresh);
-
-gulp.task('reload-pages', function () {
-  browserSyncRefresh();
-});
+gulp.task('reload-sass', ['sass'], browserSyncStream);
 
 /* Watch task */
 gulp.task('watch', function (cb) {
@@ -153,8 +129,6 @@ gulp.task('watch', function (cb) {
     function () {
       /* Avoiding the compilation of the vendor styles on save, faster faster faster! */
       gulp.watch(sourcePaths.stylesheets + '**/*.scss', ['reload-sass']);
-      gulp.watch(sourcePaths.assets      + 'fonts/',    ['reload-fonts']);
-      gulp.watch(sourcePaths.assets      + 'images/',   ['reload-images']);
 
       gutil.log(gutil.colors.bgGreen('Watching for changes...'));
       return cb();
@@ -168,8 +142,6 @@ gulp.task('build', function (cb) {
   return runSequence(
     'clean', [
       'sass',
-      'fonts',
-      'images'
     ], cb);
 });
 
