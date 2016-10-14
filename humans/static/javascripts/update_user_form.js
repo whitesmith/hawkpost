c473132394a817d7e92565a38ccc56f08edba413
MariCerv <mari.cerv@gmail.com>
2016-10-14 16:07:28 +0100

    indentation fixed

--- humans/static/javascripts/update_user_form.js
+++ humans/static/javascripts/update_user_form.js
@@ -7,26 +7,26 @@ $(document).ready(function(){
   }
 
   $("#id_keyserver_url").on("input",function(){
-  if($("#id_keyserver_url").val().length == 0) {
-    $("#id_public_key").attr("disabled",false);
-  } else {
-    $("#id_public_key").attr("disabled",true);
-  }
-});
+    if($("#id_keyserver_url").val().length == 0) {
+      $("#id_public_key").attr("disabled",false);
+    } else {
+      $("#id_public_key").attr("disabled",true);
+    }
+  });
 
-$("#id_public_key").on("input",function(){
-  if($("#id_public_key").val().length == 0) {
-    $("#id_keyserver_url").attr("disabled",false);
-  } else {
-    $("#id_keyserver_url").attr("disabled",true);
-  }
-});
+  $("#id_public_key").on("input",function(){
+    if($("#id_public_key").val().length == 0) {
+      $("#id_keyserver_url").attr("disabled",false);
+    } else {
+      $("#id_keyserver_url").attr("disabled",true);
+    }
+  });
 
-$(".form__block label").addClass("smalltext");
-$(".form__block input[type=text]").addClass("text padding-settings");
+  $(".form__block label").addClass("smalltext");
+  $(".form__block input[type=text]").addClass("text padding-settings");
 
-$("#id_server_signed").change(function() {
-  $('.server-signed-info-js .popuptext').toggleClass('show');
-});
+  $("#id_server_signed").change(function() {
+    $('.server-signed-info-js .popuptext').toggleClass('show');
+  });
 });
 
