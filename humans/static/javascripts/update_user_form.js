1934f764a5aaf19f9dea36de454a63c26194444e
MariCerv <mari.cerv@gmail.com>
2016-10-14 16:03:02 +0100

    JS fixed + Server signed checkbox fixed

--- humans/static/javascripts/update_user_form.js
+++ humans/static/javascripts/update_user_form.js
@@ -1,7 +1,3 @@
-$(".form__block label").addClass("smalltext");
-$(".form__block input[type=text]").addClass("text padding-settings");
-
-
 $(document).ready(function(){
   if($("#id_keyserver_url").val().length != 0) {
     $("#id_public_key").attr("disabled",true);
@@ -25,8 +21,12 @@ $("#id_public_key").on("input",function(){
     $("#id_keyserver_url").attr("disabled",true);
   }
 });
-});
+
+$(".form__block label").addClass("smalltext");
+$(".form__block input[type=text]").addClass("text padding-settings");
 
 $("#id_server_signed").change(function() {
   $('.server-signed-info-js .popuptext').toggleClass('show');
 });
+});
+
