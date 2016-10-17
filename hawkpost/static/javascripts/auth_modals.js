c7c1af03927617d57ac2ffce635423a9d196947e
MariCerv <mari.cerv@gmail.com>
2016-10-17 13:58:53 +0100

    Login + Sign up modals fixed
    - new js file to fix inputs
    - font colors fixed
    - github log in fixed

---
+++ hawkpost/static/javascripts/auth_modals.js
@@ -0,0 +1,5 @@
+$("#id_email").addClass("text padding-modals1");
+$("#id_password1").addClass("text padding-modals1");
+$("#id_password2").addClass("text padding-modals1");
+$("#id_login").addClass("text padding-modals1");
+$("#id_password").addClass("text padding-modals1");
