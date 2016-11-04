6a3d99c4145cfdf7b2d8a845d94dc04113caa110
MariCerv <mari.cerv@gmail.com>
2016-11-04 12:43:30 +0000

    final settings fixes
    - deleted the old way to config keyserver or public key
    - fixed the animation
    -fixed save button
    - fixed navigation

--- humans/static/javascripts/update_user_form.js
+++ humans/static/javascripts/update_user_form.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
-  if($("#id_keyserver_url").val().length != 0) {
-    $("#id_public_key").attr("disabled",true);
-  }
-  else if($("#id_public_key").val().length != 0) {
-    $("#id_keyserver_url").attr("disabled",true);
-  }
-
-  $("#id_keyserver_url").on("input",function(){
-    if($("#id_keyserver_url").val().length == 0) {
-      $("#id_public_key").attr("disabled",false);
-    } else {
-      $("#id_public_key").attr("disabled",true);
-    }
-  });
+  
 
   $(".form__block label").addClass("smalltext");
   $(".form__block input[type=text]").addClass("text padding-settings");
@@ -20,38 +7,38 @@ $(document).ready(function(){
 
   $("#tab1").click(function(){
     //slide up all the link lists
-    $("#section1").slideDown();
-    $("#section2").slideUp();
-    $("#section3").slideUp();
+    $("#section1").show();
+    $("#section2").hide();
+    $("#section3").hide();
     $("#tab1").addClass("active");
     $("#tab2").removeClass("active");
     $("#tab3").removeClass("active");
   });
   $("#tab2").click(function(){
     //slide up all the link lists
-    $("#section2").slideDown();
-    $("#section1").slideUp();
-    $("#section3").slideUp();
+    $("#section2").show();
+    $("#section1").hide();
+    $("#section3").hide();
     $("#tab2").addClass("active");
     $("#tab1").removeClass("active");
     $("#tab3").removeClass("active");
   });
   $("#tab3").click(function(){
     //slide up all the link lists
-    $("#section3").slideDown();
-    $("#section1").slideUp();
-    $("#section2").slideUp();
+    $("#section3").show();
+    $("#section1").hide();
+    $("#section2").hide();
     $("#tab3").addClass("active");
     $("#tab1").removeClass("active");
     $("#tab2").removeClass("active");
   });
   $('.radio_button').on('change', function() {
     if ($('.radio_button:checked').val() == "keyserver") {
-      $("#keyserver_url").slideDown();
-      $("#public_key").slideUp();
+      $("#keyserver_url").show();
+      $("#public_key").hide();
     } else {
-      $("#keyserver_url").slideUp();
-      $("#public_key").slideDown();
+      $("#keyserver_url").hide();
+      $("#public_key").show();
     }
   });
 });
