7fbdb50111e217e0e9083949fe32b33c379088ca
Gonçalo Valério <dethos@users.noreply.github.com>
2023-04-25 00:35:35 +0100

    Update common.py
    Change static files storage

--- hawkpost/settings/common.py
+++ hawkpost/settings/common.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media Files
 
